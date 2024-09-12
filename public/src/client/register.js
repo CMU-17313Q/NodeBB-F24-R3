1473208f13fc350046028eaaeb8984308ad0b61a
Ashwaq Taib <ashwaqtaib@dyn-172-20-99-206.qatar.cmu.local>
2024-09-12 17:46:25 +0300

    done with the new name issue

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -114,6 +114,7 @@ define('forum/register', [
 		$('#username').trigger('focus');
 	};
 
+	
 	function validateUsername(username, callback) {
 		callback = callback || function () {};
 
@@ -135,9 +136,8 @@ define('forum/register', [
 				if (results.every(obj => obj.status === 'rejected')) {
 					showSuccess(usernameInput, username_notify, successIcon);
 				} else {
-					showError(usernameInput, username_notify, '[[error:username-taken]]');
+					showError(usernameInput, username_notify, `[[error:username-taken]] Maybe try ${usernameInput.val()}suffix`);
 				}
-
 				callback();
 			});
 		}
