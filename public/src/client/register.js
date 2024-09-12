92158d32c206bfd62f51031cf67d258128f4b19f
Fatou GUEYE <fatougueye@Fatous-MacBook-Air-7.local>
2024-09-12 18:53:24 +0300

    fixed the output given when username is taken

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -135,7 +135,9 @@ define('forum/register', [
 				if (results.every(obj => obj.status === 'rejected')) {
 					showSuccess(usernameInput, username_notify, successIcon);
 				} else {
-					showError(usernameInput, username_notify, '[[error:username-taken]]');
+					showError(username_notify, 
+					'[[error:username-taken, ' + 
+					username + 'suffix' + ']]');
 				}
 
 				callback();
